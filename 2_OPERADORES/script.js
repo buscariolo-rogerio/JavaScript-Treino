39ef5f21fca2d32f3902462b87dad129a083d17f
Rogerio Buscariolo Da Silva <rogeriosilva-ieg@instituto.ad>
2025-08-06 18:09:15 -0300

    Primeiro commit do Treino de JS

---
+++ 2_OPERADORES/script.js
@@ -0,0 +1,23 @@
+//TIPO NUMBER:
+console.log(typeof 2);
+
+//OPERAÇÕES:
+console.log((2+2)**4);
+console.log(2*7);
+
+//SPECIAL NUMBERS:
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(12 * 'vinicius');
+
+//STRING:
+window.alert(`YGONA MOURA`);
+window.alert('Turquia');
+window.alert("Vinicius");
+
+//SIMBOLOS ESPECIAIS DE STRING:
+console.log("Testando a \nquebra de linha");
+console.log(`Podemos executar qualquer coisa aqui ${console.log("Cachorro quente")}`);
+
+console.log(15<19)
